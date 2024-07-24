f6ed4645cf70633692d45b61b8f746c668726a3f
dev-pine2 <campst03@baeksin.ms.kr>
2024-07-24 12:13:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import threading
 import time
 st.title('검열없는 채팅방')
 name = st.text_input("이름을 입력하세요: ")
-text = st.text_input("문자를 입력하세요: ")
 place = st.empty()
 if st.button('대화 전송||대화확인'):
   if text:
@@ -17,5 +16,6 @@ def check():
       place.text(f.read())
     time.sleep(1)
 check()
+text = st.text_input("문자를 입력하세요: ")
 # p2 = threading.Thread(target=check)
 # p2.start()
