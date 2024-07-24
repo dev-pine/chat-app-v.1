3635b7a4e7251f7032bcd015f81638ae94be981c
dev-pine2 <campst03@baeksin.ms.kr>
2024-07-24 11:44:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ text = st.text_input("문자를 입력하세요: ")
 if st.button('대화 전송||대화확인'):
   if text:
     with open("d.txt","w") as f:
-      f.write(name+":"+text)
+      f.write(name+":"+text+"\n")
     with open("d.txt","r") as f:
       st.write(f.read())
