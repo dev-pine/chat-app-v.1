ec191e8604b0110bd6396c3cbf5da61aa725c77a
dev-pine2 <campst03@baeksin.ms.kr>
2024-07-24 12:01:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 st.title('나의 첫 웹서비스')
 name = st.text_input("이름을 입력하세요: ")
 text = st.text_input("문자를 입력하세요: ")
-button = st.button('대화 전송||대화확인'):
+button = st.button('대화 전송||대화확인')
 def send():
   if button:
     if text:
