e82618fba5efed862ec90ec97a0bf7caa9b205af
dev-pine2 <campst03@baeksin.ms.kr>
2024-07-24 12:07:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 st.title('나의 첫 웹서비스')
 name = st.text_input("이름을 입력하세요: ")
 text = st.text_input("문자를 입력하세요: ")
-
+place = st.empty()
 if st.button('대화 전송||대화확인'):
   if text:
     with open("d.txt","a") as f:
@@ -12,8 +12,9 @@ if st.button('대화 전송||대화확인'):
       f.write("\n")
 def check():
   while True:
+    place.empty()
     with open("d.txt","r") as f:
-      st.write(f.read()+"\n")
+      place.text(f.read())
     time.sleep(1)
 check()
 # p2 = threading.Thread(target=check)
