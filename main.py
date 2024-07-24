ab558fe4f592befadccdbfe59329afd42134c783
dev-pine2 <campst03@baeksin.ms.kr>
2024-07-24 11:56:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import streamlit as st
+import threading
+import time
 st.title('나의 첫 웹서비스')
 name = st.text_input("이름을 입력하세요: ")
 text = st.text_input("문자를 입력하세요: ")
-while True:
+def send():
   if st.button('대화 전송||대화확인'):
     if text:
       with open("d.txt","a") as f:
         f.write(name+":"+text)
         f.write("\n")
+def check():
   with open("d.txt","r") as f:
-    st.write(f.read())
+    st.write(f.read()+"\n")
+p1 = threading.Thread(target=send)
+p2 = threading.Thread(target=check)
+p1.start()
+p2.start()
