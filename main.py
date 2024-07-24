e0fe33f6340ae7031baf2f2cc0758f1bb075d670
dev-pine2 <campst03@baeksin.ms.kr>
2024-07-24 11:51:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import streamlit as st
 st.title('나의 첫 웹서비스')
 name = st.text_input("이름을 입력하세요: ")
 text = st.text_input("문자를 입력하세요: ")
-
-if st.button('대화 전송||대화확인'):
-  if text:
-    with open("d.txt","a") as f:
-      f.write(name+":"+text)
-      f.write("\n")
+while True:
+  if st.button('대화 전송||대화확인'):
+    if text:
+      with open("d.txt","a") as f:
+        f.write(name+":"+text)
+        f.write("\n")
   with open("d.txt","r") as f:
     st.write(f.read())
